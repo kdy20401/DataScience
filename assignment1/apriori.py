5857ad830d6ac0e216657f83248cd8361300bf55
강덕영 <rkdejrdud88@MacBook-Pro.local>
2022-04-10 03:11:18 +0900

    implemented a decision tree

--- assignment1/apriori.py
+++ assignment1/apriori.py
@@ -4,77 +4,73 @@ from itertools import combinations
 from itertools import product
 
 
-DEBUG = False
+DEBUG_MODE = False
+
+ROUND_AT = 2
 context = decimal.getcontext()
 context.rounding = decimal.ROUND_HALF_UP
 
-min_support = round(decimal.Decimal(sys.argv[1]), 2)
+min_support = round(decimal.Decimal(sys.argv[1]), ROUND_AT)
 input_file = sys.argv[2]
 output_file = sys.argv[3]
 
 database = []
-frequent_item = []
+db_size = None
 infrequent_item = []
 candidate_support = {}
 frequent_item_support = {}
+item_size = 1
 
 # first database scan
 with open(input_file, 'r') as f:   
     for i, line in enumerate(f.readlines()):
         transaction = set()
         for item in line.split():
-            candidate_support[tuple([item])] = 0
+            candidate_support[tuple([item])] = 0 # note [item]
             transaction.add(item)            
         database.append(transaction)
 db_size = len(database)
 
-if DEBUG:
+if DEBUG_MODE:
     print('database:')
     for transaction in database:
         print(transaction)
 
 # calculate support of item with length one
-for item in candidate_support.keys():
+for item in list(candidate_support):
     for transaction in database:
         if transaction & set(item) == set(item):
             candidate_support[item] += 1
-for item in candidate_support.keys():
-    if round(decimal.Decimal(100 * candidate_support[item] / db_size), 2) < min_support:
+
+# check if item satisfies the minimum support
+for item in list(candidate_support):
+    support = round(decimal.Decimal(100 * candidate_support[item] / db_size), ROUND_AT)
+    if support < min_support:
         infrequent_item.append(set(item))
 for item in infrequent_item:
     del candidate_support[tuple(item)]
 
-for candi, sup in candidate_support.items():
-    frequent_item_support[candi] = sup
+frequent_item_support = {c:s for c, s in candidate_support.items()}
+item_size += 1
 
 # get frequent items
-while len(candidate_support.keys()) > 0:
+while len(list(candidate_support)) > 0:
     # 1. generating candidate
-    next_candidate_support = {}
-    next_candidate = []
-    for i in combinations(candidate_support.keys(), 2):
-        c = set(i[0]).union(set(i[1]))
-        # do not include candidate which has already been determined to frequent item at previous iterations
-        if c not in frequent_item:
-            frequent_item.append(c)
-            # make candidates independent
-            if c not in next_candidate:
-                next_candidate.append(c)
-    
+    unique_key = set([key for item in list(candidate_support) for key in item])
+    next_candidate = list(map(set, list(combinations(unique_key, item_size))))
+
     # 2. candidate pruning: remove candidate which contains an infreqneut item
     candidate_with_infrequent_item = []
     for c in next_candidate:
         is_subset_frequent = True
-        for d in infrequent_item:
-            if c & d == d:
+        for i in infrequent_item:
+            if c & i == i:
                 candidate_with_infrequent_item.append(c)
                 break
-    for c in candidate_with_infrequent_item:
-        next_candidate.remove(c)
-    for c in next_candidate:
-        next_candidate_support[tuple(c)] = 0
+    next_candidate = [c for c in next_candidate if c not in candidate_with_infrequent_item]
+    next_candidate_support = {tuple(c): 0 for c in next_candidate}
     
-    # 3. removing candidate under the minimum support
+    # 3. calculate support and remove candidate under the minimum support
     for c in next_candidate:
         for transaction in database:
             if transaction & c == c:
@@ -86,27 +82,23 @@ while len(candidate_support.keys()) > 0:
             candidate_under_min_support.append(c)
     for c in candidate_under_min_support:
         del next_candidate_support[tuple(c)]
-    
-    # keep frequent item set clean
-    for c in candidate_with_infrequent_item:
-        frequent_item.remove(c)
-    for c in candidate_under_min_support:
-        frequent_item.remove(c)
 
-    for k, v in candidate_support.items():
-        frequent_item_support[k] = v
+    # update frequent item dictionary
+    for c, s in next_candidate_support.items():
+        frequent_item_support[c] = s
     candidate_support = next_candidate_support
+    item_size += 1
 
-if DEBUG:
+if DEBUG_MODE:
     print('\n================frequent items====================')
-    for k in frequent_item_support.keys():
+    for k in list(frequent_item_support):
         print(k, ':', frequent_item_support[k], '\n')
-    print('number of frequent items:', len(frequent_item_support.keys()))
+    print('number of frequent items:', len(list(frequent_item_support)))
     print('==================================================\n')
 
 # get association rule from frequent items
 association_rule = []
-for item in frequent_item_support.keys():
+for item in list(frequent_item_support):
     item_length = len(item)
     if item_length == 1:
         continue
@@ -121,7 +113,7 @@ for item in frequent_item_support.keys():
             if len(set(i) & set(j)) == 0:
                 association_rule.append((i, j))
 
-if DEBUG:
+if DEBUG_MODE:
     print('association rule:')
     print('(item set) -> (associative item set)    support    confidence')
 
@@ -131,7 +123,7 @@ for p, q in association_rule:
     pq = p + q
     pq_support, p_support = 0, 0
 
-    for item in frequent_item_support.keys():
+    for item in list(frequent_item_support):
         if set(pq) == set(item):
             pq_support = frequent_item_support[item]
         if set(p) == set(item):
@@ -141,7 +133,7 @@ for p, q in association_rule:
     association_confidence = round(decimal.Decimal(100 * pq_support / p_support), 2)
     
     if association_support >= min_support and association_confidence >= min_support:
-        if DEBUG: print(p, '->', q, association_support, association_confidence)
+        if DEBUG_MODE: print(p, '->', q, association_support, association_confidence)
 
         item_set = ''
         associative_item_set = ''
---
+++ assignment2/dt.py
@@ -0,0 +1,193 @@
+import sys
+import numpy as np
+import pandas as pd
+
+
+class Node:
+    '''
+    Tree node for decision tree.
+    
+    Instance Variables:
+        isLeaf(bool): indicate whether a node is internal or leaf
+        label(str): the label value in leaf node. internal node doesn't have label.
+        splitting_attribute(str): the attribute which split the current node (ex, 'age')
+        splitting_criteria(numpy.ndarray): attribute's values. (ex, ['<=30', '30...40'])
+        each criterion indicates the path to the child nodes. acts like a pointer to the child nodes
+        children(dict): key is criterion and value is Node.
+    
+    Instance Method:
+        set_splitting_criteria: make branch(pointer) to child nodes using criteria
+        check: decide which branch to follow and return corresponding child node.
+        when reached to leaf node, return label.
+    '''
+
+    def __init__(self, isLeaf=False, label=None):
+        self.isLeaf = isLeaf
+        self.label = label
+
+    def is_leaf(self):
+        return self.isLeaf
+
+    def attach_sibling(self, criterion, node):
+        self.children[criterion] = node
+    
+    def set_splitting_attribute(self, attribute, criteria):
+        self.splitting_attribute = attribute
+        self.splitting_criteria = criteria
+        self.children = {c: None for c in criteria}
+
+    def set_label(self, label):
+        self.isLeaf = True
+        self.label = label
+
+    def get_label(self):
+        return self.label
+    
+    def check(self, data):
+        if self.isLeaf:
+            return self.label
+
+        criterion = data[self.splitting_attribute]
+        return self.children[criterion]
+
+
+def ID3(data, attribute, label_column):
+    '''
+    Calculate sum of the entropy of nodes splitted by the attribute.
+    
+    Parameters:
+        data(pandas.Dataframe): tablular data
+        label_column(str): label attribute
+        attribute(str): target attribute to split data
+    '''
+
+    delta = 1e-7 # to avoid Nan
+    table = data.groupby([attribute, label_column])[attribute].count().unstack().fillna(0).to_numpy()
+    coeff = np.sum(table, axis=1) / np.sum(table)
+    p = table / np.sum(table, axis=1).reshape(-1, 1)
+    log_p = np.log2(p + delta)
+    entropy = -np.sum(p * log_p, axis=1)
+    return np.sum(coeff * entropy)
+
+
+def find_the_best_splitting_attribute(data, attribute_list, label_column, attribute_selection_method):
+    '''
+    Find the optimal splitting attribute and return the attribute criteria.
+    
+    Parameters:
+        data(pandas.Dataframe): tablular data
+        attribute_list(dict): key is attribute, value is list of unique values. this parameter
+        saves the kinds of value the specific attribute contains.
+        label_column(str): label attribute
+        attribute_selection_method(function): function used to calculate impurity. (in this code, ID3)
+    '''
+
+    entropy = {}  # key: attribute, value: sum of data's entropy splitted by that attribute
+
+    for attribute in attribute_list:
+        entropy[attribute] = attribute_selection_method(data, attribute, label_column)
+
+    # find the minimum entropy
+    entropy = sorted(entropy.items(), key=lambda x: x[1])
+    splitting_attribute = entropy[0][0]
+    return splitting_attribute
+
+
+def generate_decision_tree(data, attribute_list, label_column, attribute_selection_method):
+    '''
+    Generate a decision tree based on the data using an attribute selection method.
+    
+    Parameters:
+        data(pandas.Dataframe): tablular data
+        attribute_list(dict): key is attribute, value is list of unique values.
+        this parameter is saving the kinds of value the specific attribute contains.
+        label_column(str): label attribute
+        attribute_selection_method(function): function used to calculate impurity. (in this code, ID3)
+    '''
+    
+    node = Node()
+
+    # samples in the data consists of only one class
+    unique_label_value = data[label_column].unique()
+    if len(unique_label_value) == 1:
+        label = unique_label_value[0]
+        node.set_label(label)
+        return node
+        
+    # there is no splitting attribute
+    if len(attribute_list) == 0:
+        label = data[label_column].value_counts().index[0] # majority voting
+        node.set_label(label)
+        return node
+
+    # find the best attribute to split node
+    splitting_attribute = find_the_best_splitting_attribute(data, attribute_list, label_column, attribute_selection_method)
+    node.set_splitting_attribute(splitting_attribute, attribute_list[splitting_attribute])
+
+    # remove the selected splitting attribute from attribute list
+    splitting_criteria = attribute_list.pop(splitting_attribute)
+
+    # split the current node by the splitting criterion
+    for criterion in splitting_criteria:
+        row_idx = data[splitting_attribute] == criterion
+        sub_data = data[row_idx].reset_index(drop=True)
+
+        # splitted node is empty
+        if sub_data.shape[0] == 0:
+            label = data[label_column].value_counts().index[0] # majority voting 
+            child_node = Node(isLeaf=True, label=label)
+            node.attach_sibling(criterion, child_node)
+        else:
+            # recursive call starts
+            # note: pass a copy of dictionary to the argument 
+            node.attach_sibling(criterion, generate_decision_tree(sub_data, attribute_list.copy(), label_column, attribute_selection_method))
+
+    return node
+
+
+def predict_label(decision_tree, data):
+    '''
+    Predict data's label using decision tree.
+    
+    Parameters:
+        decision_tree(Node): root node of decision tree
+        data(dict): e,g, {'age': '<=30', 'income': 'high',,, 'Class:buys_computer': 'no'}
+    '''
+
+    node = decision_tree.check(data)
+
+    while not node.is_leaf():
+        node = node.check(data)
+
+    return node.get_label()
+
+
+def main():
+    train_data_file = sys.argv[1]
+    test_data_file = sys.argv[2]
+    output_file = sys.argv[3]
+
+    train_data = pd.read_csv(f'./data/{train_data_file}', sep='\t')
+    test_data = pd.read_csv(f'./data/{test_data_file}', sep='\t')
+
+    columns_without_label = train_data.columns[:-1].to_numpy()
+    label_column = train_data.columns[-1]
+    unique_values = []
+    for col in columns_without_label:
+        unique_values.append(train_data[col].unique())
+    attribute_list = {col: vals for col, vals in zip(columns_without_label, unique_values)}
+
+    # generate decision tree using train data
+    dt = generate_decision_tree(train_data, attribute_list, label_column, ID3)
+
+    # predict labels 
+    predictions = []
+    for i in range(test_data.shape[0]):
+        row = test_data.iloc[i].to_dict()
+        label = predict_label(dt, row)
+        predictions.append(label)
+    test_data[label_column] = predictions
+    test_data.to_csv(output_file, index=False, sep='\t')
+
+if __name__ == '__main__':
+    main()
